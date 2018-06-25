bf528138f940a5ecb77e8a420e1ec0f9744afea2
Scheercuzy <maxi730@gmail.com>
2018-06-25 20:17:38 +0630

    Basic Boilerplate for Redux

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
 
-import Navbar from './Layouts/Navbar'
+import Navbar from './layouts/Navbar'
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './Components/App'
+import App from './components/App'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { store } from './components/store'
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root')
+)
