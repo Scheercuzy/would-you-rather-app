6f0034c16afd1735f21d26e730dc8d598d037f40
Scheercuzy <maxi730@gmail.com>
2018-06-25 20:04:39 +0630

    Fixed navbar

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
 import Navbar from './Layouts/Navbar'
 
@@ -6,6 +7,7 @@ class App extends Component {
   render() {
     return (
     <Fragment>
+      <CssBaseline />
       <Navbar />
     </Fragment>
     )
